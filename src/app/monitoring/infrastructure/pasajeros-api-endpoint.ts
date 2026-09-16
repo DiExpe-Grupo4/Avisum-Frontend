5010863c3a4e258b77a6d763cf3b6ed403a4d476
CarlosBlancas969 <u20241a322@upc.edu.pe>
2026-09-16 16:05:12 -0500

    Update pasajeros-api-endpoint.ts

--- src/app/monitoring/infrastructure/pasajeros-api-endpoint.ts
+++ src/app/monitoring/infrastructure/pasajeros-api-endpoint.ts
@@ -1,4 +1,6 @@
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { BaseApiEndpoint } from '../../shared/infrastructure/base-api-endpoint';
 import { Pasajero } from '../domain/model/pasajero.entity';
 import { PasajeroResource, PasajerosResponse } from './pasajeros-response';
@@ -7,6 +9,16 @@ import { environment } from '../../../environments/environment';
 
 export class PasajerosApiEndpoint extends BaseApiEndpoint<Pasajero, PasajeroResource, PasajerosResponse, PasajeroAssembler> {
   constructor(http: HttpClient) {
-    super(http, environment.platformProviderApiBaseUrl + environment.platformProviderPasajerosEndpointPath, new PasajeroAssembler());
+    super(
+      http,
+      environment.platformProviderApiBaseUrl + environment.platformProviderPasajerosEndpointPath,
+      new PasajeroAssembler()
+    );
+  }
+
+  getByTurno(shiftId: number): Observable<Pasajero[]> {
+    return this.http.get<PasajeroResource[]>(`${this.endpointUrl}/shift/${shiftId}`).pipe(
+      map(list => list.map(r => this.assembler.toEntityFromResource(r)))
+    );
   }
 }
